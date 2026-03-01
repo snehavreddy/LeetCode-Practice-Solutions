c27339d663908076082271cbed190d8319b135cf
snehav <snehavreddy12@gmail.com>
2026-03-01 17:13:06 +0530

    Movie Rating

---
+++ sql/medium_1480-movie-rating.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+(
+    -- Part 1: Find the user with the most ratings
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    -- Part 2: Find the movie with highest avg rating in Feb 2020
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+);
