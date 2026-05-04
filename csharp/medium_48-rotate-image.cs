5277c525745ff6027c2789decfdbeebb7c418d34
snehav <snehavreddy12@gmail.com>
2026-05-04 08:28:26 +0530

    Auto-sync LeetCode - 2026-05-04 08:28:26

---
+++ csharp/medium_48-rotate-image.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+
+        // Step 1: Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            Array.Reverse(matrix[i]);
+        }
+    }
+}
